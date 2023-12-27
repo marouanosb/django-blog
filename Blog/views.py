39b94906ca5f72c3863c3f4ed26a3b75b3bcc092
unknown <marouanosb@gmail.com>
2023-12-27 14:48:53 +0100

    Database, migratio registering models

--- Blog/views.py
+++ Blog/views.py
@@ -1,38 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
-posts = [
-    {
-        'author': 'Merouane',
-        'title': 'Blog post 1',
-        'content': 'First post content',
-        'date': '25/12/2023'
-     },
-     {
-        'author': 'Merouane',
-        'title': 'Blog post 2',
-        'content': '2nd post content',
-        'date': '24/12/2023'
-     },
-     {
-        'author': 'Amine',
-        'title': 'Blog post 3',
-        'content': '3rd post content',
-        'date': '23/12/2023'
-     },
-     {
-        'author': 'Hamid',
-        'title': 'Blog post 4',
-        'content': '4th post content',
-        'date': '22/12/2023'
-     }
-]
-
 def home(request):
     context = {
         #'title' : 'home',
-        'posts' : posts
+        'posts' : Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
